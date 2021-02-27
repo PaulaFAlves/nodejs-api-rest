c90f71fa12b15c8a85b30cf824dfe5b8e0c19e0e
paulafalves <paula.alves@elysios.com.br>
2021-02-27 15:55:38 -0300

    Include answer controller, refactor errors and tests

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -1,4 +1,5 @@
 import { response } from 'express';
+import { getConnection } from 'typeorm'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -13,23 +14,29 @@ describe("Surveys", () => {
             console.log(err)
         }
     })
-    
-    it ("should be able to create a new survey", async () => {
+
+    afterAll(async () => {
+        const connection = getConnection()
+        await connection.dropDatabase()
+        await connection.close()
+    })
+
+    it("should be able to create a new survey", async () => {
         const response = await request(app).post("/surveys")
-        .send({
-            title: "title example",
-            description: "example description"
-        })
+            .send({
+                title: "title example",
+                description: "example description"
+            })
         expect(response.status).toBe(201)
         expect(response.body).toHaveProperty("id")
     })
 
-    it ("should be able to get all surveys", async () => {
+    it("should be able to get all surveys", async () => {
         await request(app).post("/surveys")
-        .send({
-            title: "title example",
-            description: "example description"
-        })
+            .send({
+                title: "title example",
+                description: "example description"
+            })
         const response = await request(app).get("/surveys")
 
         expect(response.body.length).toBe(2)
